b5294367f781af6c027dcbfa12efa77f3019a6ed
patovara <patvr.code@gmail.com>
2026-04-15 11:36:09 -0500

    style(presupuesto): adjust pdf watermark again for 3rd time

--- lib/features/presupuesto/presentation/presupuesto_page.dart
+++ lib/features/presupuesto/presentation/presupuesto_page.dart
@@ -540,7 +540,7 @@ class PresupuestoPage extends ConsumerWidget {
               ? (_) => pw.Positioned.fill(
                     child: pw.Center(
                       child: pw.Opacity(
-                        opacity: 0.03, //se ajusto la opacidad para que la marca de agua sea más sutil
+                        opacity: 0.1, //se ajusto la opacidad para que la marca de agua sea más sutil
                         child: pw.Image(watermark, width: 380, fit: pw.BoxFit.contain),
                       ),
                     ),
